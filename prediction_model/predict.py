24b12e96a4e11c6c9a2cb2fa051850c97852e7f2
lap <luuchilap@gmail.com>
2024-08-16 00:09:17 +0700

    c

--- prediction_model/predict.py
+++ prediction_model/predict.py
@@ -14,20 +14,20 @@ from prediction_model.processing.data_handling import load_pipeline,load_dataset
 
 classification_pipeline = load_pipeline(config.MODEL_NAME)
 
-def generate_predictions(data_input):
-    data = pd.DataFrame(data_input)
-    pred = classification_pipeline.predict(data[config.FEATURES])
-    output = np.where(pred==1,'Y','N')
-    result = {"prediction":output}
-    return result
-
-# def generate_predictions():
-#     test_data = load_dataset(config.TEST_FILE)
-#     pred = classification_pipeline.predict(test_data[config.FEATURES])
+# def generate_predictions(data_input):
+#     data = pd.DataFrame(data_input)
+#     pred = classification_pipeline.predict(data[config.FEATURES])
 #     output = np.where(pred==1,'Y','N')
-#     print(output)
-#     #result = {"Predictions":output}
-#     return output
+#     result = {"prediction":output}
+#     return result
+
+def generate_predictions():
+    test_data = load_dataset(config.TEST_FILE)
+    pred = classification_pipeline.predict(test_data[config.FEATURES])
+    output = np.where(pred==1,'Y','N')
+    print(output)
+    result = {"Predictions":output}
+    return output
 
 if __name__=='__main__':
     generate_predictions()
